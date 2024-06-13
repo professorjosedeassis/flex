86e7a6a98a324d844f50eecc790c8da77f3657f3
Professor José de Assis <jose.afilho3@senacsp.edu.br>
2024-06-13 18:58:11 -0300

    flex

--- app.js
+++ app.js
@@ -4,42 +4,21 @@
  * @link https://joseassis.com.br/
  */
 
-let gasolina, etanol, kmGasolina, kmEtanol, vantagem
-
 function calcular() {
     // pegar valores dos inputs
-    gasolina = Number(document.getElementById('gasolina').value)
-    etanol = Number(document.getElementById('etanol').value)
-    kmGasolina = Number(document.getElementById('kmGasolina').value)
-    kmEtanol = Number(document.getElementById('kmEtanol').value)
-   // console.log(gasolina, etanol, kmGasolina, kmEtanol)
-
-    // validação dos campos
-    if (gasolina === 0) {
-        alert("Preencha o valor da gasolina")        
-        document.getElementById('gasolina').focus()
-    } else if (etanol === 0) {
-        alert("Preencha o valor do etanol") 
-        document.getElementById('etanol').focus()
-    } else if (kmGasolina === 0) {
-        alert("Preencha o consumo médio de gasolina (km/L)") 
-        document.getElementById('kmGasolina').focus()
-    } else if (kmEtanol === 0) {
-        alert("Preencha o consumo médio de etanol (km/L)") 
-        document.getElementById('kmEtanol').focus()
-    } else {       
-        // calcular a vantagem
-        vantagem = (etanol / kmEtanol) / (gasolina / kmGasolina)
-        //console.log(vantagem)
-        // exbir o melhor custo/benefíco   
-        if (vantagem < 1) {
-            document.getElementById("status").src = "./img/etanol.png"
-        } else {           
-            document.getElementById("status").src = "./img/gasolina.png"
-        }
+    let gasolina = Number(document.getElementById('gasolina').value)
+    let etanol = Number(document.getElementById('etanol').value)
+    let kmGasolina = Number(document.getElementById('kmGasolina').value)
+    let kmEtanol = Number(document.getElementById('kmEtanol').value)    
+    // calcular a vantagem
+    vantagem = (etanol / kmEtanol) / (gasolina / kmGasolina)
+    //console.log(vantagem)
+    // exbir o melhor custo/benefíco   
+    if (vantagem > 1) {
+        document.getElementById("status").src = "./img/gasolina.png"       
+    } else if (vantagem < 1) {
+         document.getElementById("status").src = "./img/etanol.png"
+    } else {
+        document.getElementById("status").src = "./img/flex.png"
     }
-}
-
-function limpar() {
-    document.getElementById("status").src = "./img/flex.png"
 }
